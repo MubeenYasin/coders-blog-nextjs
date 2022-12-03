c030a56c1148bca8c157da32793884f7e5693623
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-12-03 22:44:15 +0300

    At 1:45 Hours

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <div className='w-10/12 mx-auto font-sans'>
+      <nav>Navigation Bar</nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>FOOTER</footer>
+    </div>
+  </>
 }
